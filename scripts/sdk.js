1e41479061dbd1b7c0ae93c8ea4303355aabd64f
mecurialminds <39082059+mecurialminds@users.noreply.github.com>
2020-05-12 16:54:21 +0500

    Update sdk.js

--- scripts/sdk.js
+++ scripts/sdk.js
@@ -2674,6 +2674,8 @@ function WebLiveChatListener() {
 				{
 					console.log("goodbye");
  					document.getElementById('botplatformchatchat').disabled=true;
+					this.minimizeBox();
+					return false;
 				}
 			}
 			if (this.speak) {
