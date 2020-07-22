cce1536859b63eb31e5a754a5e17e59b9c0d0874
mecurialminds <39082059+mecurialminds@users.noreply.github.com>
2020-07-22 12:21:31 +0500

    Update sdk.js

--- scripts/sdk.js
+++ scripts/sdk.js
@@ -2114,7 +2114,7 @@ function WebLiveChatListener() {
 					 myTimer = setTimeout(function() {
 						console.log("timer started")
 						document.getElementById('botplatformchatchat').disabled = true;
-						this.socket.onclose();				 
+						socket.close();				 
 						return false;
 					    }, 10000);
 					myTimerCheck=true;
